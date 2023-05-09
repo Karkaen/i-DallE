8777301feeb84227ab36402c9c7c37e6da27b771
Karkaen <104906015+Karkaen@users.noreply.github.com>
2023-05-09 04:41:43 +0300

    Update postRoutes.js

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -10,10 +10,11 @@ const router = express.Router();
 
 // Configuration 
 cloudinary.config({
-  cloud_name: CLOUDINARY_CLOUD_NAME,
-  api_key: CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
 
 
 router.route('/').get(async (req, res) => {
